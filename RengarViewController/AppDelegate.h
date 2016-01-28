bf19fe1224bdf9f5d330f5d45a0cbea528670dc6
peiwei <cpwduck@gmail.com>
2016-01-28 22:28:37 +0800

    Initial Commit

---
+++ RengarViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RengarViewController
+//
+//  Created by chenpeiwei on 1/28/16.
+//  Copyright © 2016 chenpeiwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RengarViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RengarViewController
+//
+//  Created by chenpeiwei on 1/28/16.
+//  Copyright © 2016 chenpeiwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
